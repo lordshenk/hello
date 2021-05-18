a8e7e8194c21b7201e7b253723c2a7e0151a031a
Shen <suusoft.khanhbd@gmail.com>
2021-05-18 23:59:02 +0700

    refactor: projects to skills

--- script.js
+++ script.js
@@ -1,49 +1,56 @@
-const main = document.getElementsByClassName('main')[0];
-const title = document.getElementsByTagName('h3')[0];
-const menu = document.getElementsByClassName('menu')[0];
-const about = document.getElementsByClassName('about')[0];
-const contact = document.getElementsByClassName('contact')[0];
-const projects = document.getElementsByClassName('projects')[0];
+const main = document.getElementsByClassName("main")[0];
+const title = document.getElementsByTagName("h3")[0];
+const menu = document.getElementsByClassName("menu")[0];
+const about = document.getElementsByClassName("about")[0];
+const contact = document.getElementsByClassName("contact")[0];
+const skills = document.getElementsByClassName("skills")[0];
 
-main.onclick = function(e) {
-	if (e.target.className === 'particles-js-canvas-el') {
-		let children = menu.children;
-		
-		menu.classList.remove('show');
-		for (let i = 0; i < children.length; ++i) {
-			children[i].classList.remove('show');
-		}
-		about.classList.remove('show');
-		contact.classList.remove('show');
-		projects.classList.remove('show');
-	}
-}
+main.onclick = function (e) {
+  if (e.target.className === "particles-js-canvas-el") {
+    let children = menu.children;
 
-title.onclick = function(e) {
-	menu.classList.add('show');
-}
+    menu.classList.remove("show");
+    for (let i = 0; i < children.length; ++i) {
+      children[i].classList.remove("show");
+    }
+    about.classList.remove("show");
+    contact.classList.remove("show");
+    skills.classList.remove("show");
+  }
+};
 
-menu.onclick = function(e) {
-	let target = e.target.textContent;
-	let children = e.currentTarget.children;
-	for (let i = 0; i < children.length; ++i) {
-		if (children[i].textContent === target) {
-			children[i].classList.add('show');
-		} else {
-			children[i].classList.remove('show');
-		}
-	}
-	if (target === 'about') {
-		about.classList.add('show');
-		projects.classList.remove('show');
-		contact.classList.remove('show');
-	} else if (target === 'contact') {
-		contact.classList.add('show');
-		projects.classList.remove('show');
-		about.classList.remove('show');
-	} else if (target === 'projects') {
-		projects.classList.add('show');
-		contact.classList.remove('show');
-		about.classList.remove('show');
-	}
-}
+title.onclick = function () {
+  menu.classList.toggle("show");
+
+  about.classList.remove("show");
+  skills.classList.remove("show");
+  contact.classList.remove("show");
+  for (let i = 0; i < menu.children; ++i) {
+    menu.children[i].classList.remove("show");
+  }
+};
+
+menu.onclick = function (e) {
+  let target = e.target.textContent;
+  let children = e.currentTarget.children;
+  for (let i = 0; i < children.length; ++i) {
+    if (children[i].textContent === target) {
+      children[i].classList.add("show");
+    } else {
+      children[i].classList.remove("show");
+    }
+  }
+  if (target === "about") {
+    about.classList.add("show");
+    skills.classList.remove("show");
+    contact.classList.remove("show");
+  } else if (target === "contact") {
+    contact.classList.add("show");
+    skills.classList.remove("show");
+    about.classList.remove("show");
+  } else if (target === "skills") {
+    skills.classList.add("show");
+    contact.classList.remove("show");
+    about.classList.remove("show");
+  }
+};
